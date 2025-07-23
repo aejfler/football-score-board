28aace63f1e3bf8506c874b03427a046835f3fab
aejfler <a.ejfler96@gmail.com>
2025-07-23 12:57:58 +0200

    Specify exceptions thrown by methods in tests and add missing test for getSummary method

--- src/test/java/service/ScoreBoardServiceImplTest.java
+++ src/test/java/service/ScoreBoardServiceImplTest.java
@@ -1,5 +1,6 @@
 package service;
 
+import com.worldcup.exception.MatchNotFoundException;
 import com.worldcup.model.Match;
 import com.worldcup.repository.impl.MatchRepositoryImpl;
 import com.worldcup.service.ScoreBoardService;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -51,14 +53,14 @@ public class ScoreBoardServiceImplTest {
 
     @Test
     void startMatch_shouldThrowException_whenTeamIsPlayingWithItself() {
-        assertThrows(Exception.class, () -> service.startMatch(HOME_TEAM, HOME_TEAM));
+        assertThrows(IllegalArgumentException.class, () -> service.startMatch(HOME_TEAM, HOME_TEAM));
     }
 
     @Test
     void updateScore_shouldModifyTeamScore() {
         service.startMatch(HOME_TEAM, AWAY_TEAM);
-        service.updateScore(HOME_TEAM, AWAY_TEAM, 2, 3);
 
+        service.updateScore(HOME_TEAM, AWAY_TEAM, 2, 3);
         List<Match> matches = service.getSummary();
 
         assertThat(matches.getFirst()).extracting(Match::getHomeScore, Match::getAwayScore).containsExactly(2,3);
@@ -82,16 +84,48 @@ public class ScoreBoardServiceImplTest {
 
     @Test
     void updateScore_shouldThrowException_whenMatchNotFound() {
-        assertThrows(Exception.class, () ->
-                service.updateScore(HOME_TEAM, AWAY_TEAM, 1, 1));
+        assertThatThrownBy(() -> service.updateScore(HOME_TEAM, AWAY_TEAM, 2, 3))
+                .isInstanceOf(MatchNotFoundException.class)
+                .hasMessage("Match not found or already finished");
     }
 
     @Test
-    void updateScore_shouldThrowExceptionWhenMatchIsFinished() {
+    void updateScore_shouldThrowException_whenMatchIsFinished() {
         service.startMatch(HOME_TEAM, AWAY_TEAM);
         service.updateScore(HOME_TEAM, AWAY_TEAM, 2, 3);
         service.finishMatch(HOME_TEAM, AWAY_TEAM);
-        assertThrows(Exception.class, () ->
-                service.updateScore(HOME_TEAM, AWAY_TEAM, 1, 1));
+        assertThatThrownBy(() -> service.updateScore(HOME_TEAM, AWAY_TEAM, 2, 4))
+                .isInstanceOf(MatchNotFoundException.class)
+                .hasMessage("Match not found or already finished");
+    }
+
+    @Test
+    void getSummary_shouldReturnEmptyList_whenNoMatchesArePresent() {
+        List<Match> summary = service.getSummary();
+
+        assertThat(summary).isEmpty();
+    }
+
+    @Test
+    void getSummary_shouldReturnMatchesSortedByTotalScoreAndLastUpdated() {
+        service.startMatch(HOME_TEAM, AWAY_TEAM);
+        service.updateScore(HOME_TEAM, AWAY_TEAM, 2, 3);
+
+        service.startMatch("USA", "England");
+        service.updateScore("USA", "England", 2, 3);
+
+        service.startMatch("Germany", "France");
+        service.updateScore("Germany", "France", 1, 1);
+
+        service.startMatch("Brazil", "Argentina");
+        service.updateScore("Brazil", "Argentina", 3, 3);
+
+        List<Match> summary = service.getSummary();
+
+        assertThat(summary).hasSize(4);
+        assertThat(summary.get(0)).extracting(Match::getHomeTeam, Match::getAwayTeam).containsExactly("Brazil", "Argentina");
+        assertThat(summary.get(1)).extracting(Match::getHomeTeam, Match::getAwayTeam).containsExactly("USA", "England");
+        assertThat(summary.get(2)).extracting(Match::getHomeTeam, Match::getAwayTeam).containsExactly(HOME_TEAM, AWAY_TEAM);
+        assertThat(summary.get(3)).extracting(Match::getHomeTeam, Match::getAwayTeam).containsExactly("Germany", "France");
     }
 }
