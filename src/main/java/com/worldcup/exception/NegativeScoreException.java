5f63adf7b1748772e04df098441ed50d8e10867d
aejfler <a.ejfler96@gmail.com>
2025-07-24 13:52:45 +0200

    Modify exception

--- src/main/java/com/worldcup/exception/NegativeScoreException.java
+++ src/main/java/com/worldcup/exception/NegativeScoreException.java
@@ -1,6 +1,6 @@
 package com.worldcup.exception;
 
-public class NegativeScoreException extends RuntimeException {
+public class NegativeScoreException extends IllegalArgumentException {
     public NegativeScoreException(int homeScore, int awayScore) {
         super("Scores must be zero or positive. Received: home = " + homeScore + ", away = " + awayScore);
     }
